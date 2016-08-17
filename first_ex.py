578133544d4390214ea2069464f9e759d154ad29
Devjiu <devjiu@gmail.com>
2016-08-17 20:55:13 +0300

    Yandex course of mashine learning

---
+++ first_ex.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas
+import re
+
+data = pandas.read_csv('D:\\Downloads\\titanic.csv')
+print data.head()
+
+
+print '---------------------------------------------------'
+print data['Name']
+print '---------------------------------------------------'
+base = np.vstack((data['Name'], data['Sex']))
+names = []
+for i in range(len(base[0])):
+    if base[1][i] == 'female':
+        names.append(base[0][i])
+first_names = []
+for name in names:
+    m = re.search('\(\w+', name)
+    if m is not None:
+        first_names.append(m.group()[1:])
+    else:
+        n = re.search('\. \w+', name)
+        first_names.append(n.group()[2:])
+print pandas.Series(first_names).value_counts()
